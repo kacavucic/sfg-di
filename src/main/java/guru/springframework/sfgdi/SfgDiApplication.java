0f14d49ccfc30a7dfca379b0733f1724e1f49688
kacavucic <vucic.kat@gmail.com>
2021-05-15 00:39:11 +0200

    primary bean added

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -15,12 +15,10 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
+		System.out.println("--------- Primary Bean");
+		System.out.println(myController.sayHello());
 
-		String greeting = myController.sayHello();
-
-		System.out.println(greeting);
-
-		System.out.println("------ Property");
+		System.out.println("--------- Property");
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
 		System.out.println(propertyInjectedController.getGreeting());
 
@@ -28,7 +26,7 @@ public class SfgDiApplication {
 		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
 		System.out.println(setterInjectedController.getGreeting());
 
-		System.out.println("-------- Constructor" );
+		System.out.println("--------- Constructor" );
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
 	}
