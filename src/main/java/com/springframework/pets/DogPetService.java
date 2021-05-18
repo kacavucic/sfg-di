2517e55ae0ee6dd5603058929b81c98d6ec8d1c1
kacavucic <vucic.kat@gmail.com>
2021-05-18 04:57:55 +0200

    property source

---
+++ src/main/java/com/springframework/pets/DogPetService.java
@@ -0,0 +1,8 @@
+package com.springframework.pets;
+
+public class DogPetService implements PetService {
+    @Override
+    public String getPetType(){
+        return "Dogs Are the Best!";
+    }
+}
